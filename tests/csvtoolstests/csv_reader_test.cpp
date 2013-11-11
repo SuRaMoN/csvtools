f8ff8169da2e07c4a57e869ae4c51ca91daa6a3a
matthias <matthias@icontroller.be>
2013-11-11 22:52:58 +0100

    Addes more tests

--- tests/csvtoolstests/csv_reader_test.cpp
+++ tests/csvtoolstests/csv_reader_test.cpp
@@ -74,4 +74,40 @@ BOOST_AUTO_TEST_CASE(test_no_final_newline_for_quoted_field)
 	BOOST_CHECK_MESSAGE(count_if(reader, constant(true)) == 2, "input should contain 2 lines");
 }
 
+BOOST_AUTO_TEST_CASE(test_intermediate_newlines_for_unquoted_field)
+{
+	csv_reader reader = csv_reader::new_from_string(L"abc,def\n\nhij,klm");
+	BOOST_CHECK_MESSAGE(count_if(reader, constant(true)) == 2, "input should contain 2 lines");
+}
+
+BOOST_AUTO_TEST_CASE(test_intermediate_newlines_for_quoted_field)
+{
+	csv_reader reader = csv_reader::new_from_string(L"abc,\"def\"\n\n\"hij\",klm");
+	BOOST_CHECK_MESSAGE(count_if(reader, constant(true)) == 2, "input should contain 2 lines");
+}
+
+BOOST_AUTO_TEST_CASE(test_intial_newlines_for_unquoted_field)
+{
+	csv_reader reader = csv_reader::new_from_string(L"\n\nabc,def\nhij,klm");
+	BOOST_CHECK_MESSAGE(count_if(reader, constant(true)) == 2, "input should contain 2 lines");
+}
+
+BOOST_AUTO_TEST_CASE(test_intial_newlines_for_quoted_field)
+{
+	csv_reader reader = csv_reader::new_from_string(L"\n\n\"abc\",def\nhij,klm");
+	BOOST_CHECK_MESSAGE(count_if(reader, constant(true)) == 2, "input should contain 2 lines");
+}
+
+BOOST_AUTO_TEST_CASE(test_end_newlines_for_unquoted_field)
+{
+	csv_reader reader = csv_reader::new_from_string(L"abc,def\nhij,klm\n\n");
+	BOOST_CHECK_MESSAGE(count_if(reader, constant(true)) == 2, "input should contain 2 lines");
+}
+
+BOOST_AUTO_TEST_CASE(test_end_newlines_for_quoted_field)
+{
+	csv_reader reader = csv_reader::new_from_string(L"abc,def\nhij,\"klm\"\n\n");
+	BOOST_CHECK_MESSAGE(count_if(reader, constant(true)) == 2, "input should contain 2 lines");
+}
+
 BOOST_AUTO_TEST_SUITE_END()
